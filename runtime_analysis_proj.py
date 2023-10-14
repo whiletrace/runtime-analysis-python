2c986c9c3a51d9e962789c20b487fb98484900bf
Trace Harris <traceh@whiletrace.com>
2023-10-14 15:58:57 -0700

    initial commit for runtime_analysis_project

---
+++ runtime_analysis_proj.py
@@ -0,0 +1,9 @@
+import random
+
+# part 1 create random integer list generator
+def random_int_list(n):
+
+    # create a list 
+    # create n number of random integers
+    # place random integers in the list 
+    # return the list 
