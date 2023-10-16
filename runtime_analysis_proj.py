8c6ec2fe88714dd608ce0475faaccc0fa8860508
whiletrace <traceh38@gmail.com>
2023-10-16 01:21:07 -0700

    added the functionality to get users input which length and num_range both are passed to the function which returns a list of random ints: length of list and range of ints based upon user input

--- runtime_analysis_proj.py
+++ runtime_analysis_proj.py
@@ -1,9 +1,26 @@
 import random
 
 # part 1 create random integer list generator
-def random_int_list(n):
-
+def random_int_list(length, num_range):
+    
     # create a list 
-    # create n number of random integers
-    # place random integers in the list 
-    # return the list 
+    random_list = []
+     # create n number of random integers
+    for i in range(length):
+        rand = random.randint(0, num_range)
+        # place random integers in the list 
+        random_list.append(rand)
+    # return the list     
+    return random_list
+   
+# part 2 get user input for size and ranget of list
+
+length = int(input('how many items would you like in the list '))
+num_range = int(input("what is the highest number represented in the list "))
+
+    
+
+
+
+# ********************execution****************
+print(random_int_list(length, num_range))
